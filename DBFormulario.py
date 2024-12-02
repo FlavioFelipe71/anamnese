53d00c3cd34d605422084e1d32ebb86a0021087a
MyProg <flaviofds77@yahoo.com.br>
2024-12-02 17:54:08 -0300

    Update DBFormulario.py

--- DBFormulario.py
+++ DBFormulario.py
@@ -11,13 +11,20 @@ import locale
 
 # Configuração inicial da página
 st.set_page_config(page_title="DB Terapeuta", layout="wide", page_icon="🌿")
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+footer {
+	
+	visibility: hidden;
+	
+	}
+footer:after {
+	content:'goodbye'; 
+	visibility: visible;
+	display: block;
+	position: relative;
+	#background-color: red;
+	padding: 5px;
+	top: 2px;
+}
 
 ##############################################################
 # HTML e CSS para a barra fixa no rodapé
